6fc80e0ae98e9fb96be4ca64054627e63f1a56cc
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-04-20 12:49:48 +1000

    added a test tab bar icon

--- portfolio/portfolio/View Controllers/TabBarViewController.swift
+++ portfolio/portfolio/View Controllers/TabBarViewController.swift
@@ -11,6 +11,15 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let image = UIImage(systemName: "square.and.arrow.up")
+        
+        // SOURCE: https://stackoverflow.com/questions/34039475/programmatically-set-the-uitabbaritem-icon-for-every-linked-viewcontroller
+        // AUTHOR: Pascal - https://stackoverflow.com/users/1912227/pascal
+        let tabBarItems = tabBar.items! as [UITabBarItem]
+        //tabBarItems[0].title = "Settings".localized
+        //tabBarItems[0].image = UIImage.fontAwesomeIconWithName(FontAwesome.Gears, textColor: UIColor.blueColor(), size: CGSizeMake(30, 30))
+        tabBarItems[0].image = image
 
         // Do any additional setup after loading the view.
     }

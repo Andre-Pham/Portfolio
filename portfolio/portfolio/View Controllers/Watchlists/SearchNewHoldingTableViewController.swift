f84c0d057e273b51e9d4cefaa0d87ad38d4a9760
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-05-02 17:21:17 +1000

    implemented webservice ticker searching

--- portfolio/portfolio/View Controllers/Watchlists/SearchNewHoldingTableViewController.swift
+++ portfolio/portfolio/View Controllers/Watchlists/SearchNewHoldingTableViewController.swift
@@ -8,82 +8,74 @@
 import UIKit
 
 class SearchNewHoldingTableViewController: UITableViewController {
+    
+    var searchResultsHoldings = [Holding]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.requestSearchTickerWebData(searchText: "NDQ")
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func requestSearchTickerWebData(searchText: String) {
+        // https://api.twelvedata.com/symbol_search?symbol=NDQ&source=docs
+        
+        // Form URL from different components
+        var requestURLComponents = URLComponents()
+        requestURLComponents.scheme = "https"
+        requestURLComponents.host = "api.twelvedata.com"
+        requestURLComponents.path = "/symbol_search"
+        requestURLComponents.queryItems = [
+            URLQueryItem(
+                name: "symbol",
+                value: searchText
+            ),
+            URLQueryItem(
+                name: "source",
+                value: "docs"
+            )
+        ]
+        
+        // Ensure URL is valid
+        guard let requestURL = requestURLComponents.url else {
+            print("Invalid URL.")
+            return
+        }
+        
+        // Occurs on a new thread
+        let task = URLSession.shared.dataTask(with: requestURL) {
+            (data, response, error) in
+            
+            // If we have recieved an error message
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            // Parse data
+            do {
+                let decoder = JSONDecoder()
+                let tickerSearchResponse = try decoder.decode(TickerSearchResults.self, from: data!)
+                print(tickerSearchResponse.data[0].symbol)
+                
+                for holding in tickerSearchResponse.data {
+                    self.searchResultsHoldings.append(
+                        Holding(
+                            ticker: holding.symbol,
+                            instrument: holding.instrument_name,
+                            exchange: holding.exchange,
+                            currency: holding.currency
+                        )
+                    )
+                }
+            
+            }
+            catch let err {
+                print(err)
+            }
+        }
+        
+        task.resume()
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ portfolio/portfolio/Web Service/TickerSearchWebData.swift
@@ -0,0 +1,19 @@
+//
+//  TickerSearchWebData.swift
+//  portfolio
+//
+//  Created by Andre Pham on 2/5/21.
+//
+
+import Foundation
+
+struct TickerSearchResults: Decodable {
+    var data: [Data]
+}
+
+struct Data: Decodable {
+    var symbol: String
+    var instrument_name: String
+    var exchange: String
+    var currency: String
+}

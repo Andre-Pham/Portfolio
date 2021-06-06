85f36dc04ac232d6fe4e20ec5f95df337efe1c30
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-06-06 10:14:26 +1000

    added disclaimer to notification settings page

--- portfolio/portfolio/View Controllers/SetNotificationSettingsViewController.swift
+++ portfolio/portfolio/View Controllers/SetNotificationSettingsViewController.swift
@@ -18,7 +18,8 @@ class SetNotificationSettingsViewController: UIViewController {
     @IBOutlet weak var contextLabel: UILabel!
     @IBOutlet weak var marketOpenDescriptionLabel: UILabel!
     @IBOutlet weak var marketCloseDescriptionLabel: UILabel!
-
+    @IBOutlet weak var disclaimerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +28,7 @@ class SetNotificationSettingsViewController: UIViewController {
         self.contextLabel.font = CustomFont.setBodyFont()
         self.marketOpenDescriptionLabel.font = CustomFont.setBodyFont()
         self.marketCloseDescriptionLabel.font = CustomFont.setBodyFont()
+        self.disclaimerLabel.font = CustomFont.setDetailFont()
         
         // Button
         saveButton.backgroundColor = UIColor(named: "GreyBlack1")

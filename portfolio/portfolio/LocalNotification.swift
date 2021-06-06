140d175525ab9a0b1f63940af4d7f7a0af335711
Andre Pham <60597159+Andre-Pham@users.noreply.github.com>
2021-06-07 04:57:45 +1000

    adjusted local notification content

--- portfolio/portfolio/LocalNotification.swift
+++ portfolio/portfolio/LocalNotification.swift
@@ -17,13 +17,21 @@ class LocalNotification: NSObject {
     
     static func startMarketStatusNotifications(time: DateComponents, identifier: String) {
         guard LocalNotification.appDelegate.notificationsEnabled else {
-            print("Notifications disabled")
             return
         }
         
         let content = UNMutableNotificationContent()
-        content.title = "Something has happened"
-        content.body = "Tap to find out what..."
+        if identifier == MARKET_OPEN_NOTIFICATION_IDENTIFIER {
+            content.title = "Markets Now Open!"
+            content.body = "Tap to view your Dashboard for today."
+        }
+        else if identifier == MARKET_CLOSE_NOTIFICATION_IDENTIIFER {
+            content.title = "Markets Have Closed"
+            content.body = "Tap to see your portfolio's and watchlists' performance for today."
+        }
+        else {
+            fatalError("startMarketStatusNotifications used incorrectly, identifier was not set to one of two options")
+        }
         content.badge = NSNumber(value: 1)
         
         // 1: Sunday, 2: Monday, etc.
